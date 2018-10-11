827fd4d05c880654b12fd5ce22c86726d68bd17c
Jean Saunie <jean.saunie@devinci.fr>
2018-10-11 23:17:45 +0200

    Clean components directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './core/components/home/home.component';
+import {HomeComponent} from './core/components/home.component';
 
 const routes: Routes = [
   {path: 'admin', loadChildren: './admin/admin.module#AdminModule'},
--- src/app/core/components/index.ts
+++ src/app/core/components/index.ts
@@ -1,4 +1,4 @@
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './home.component';
 
 const Components = [
   HomeComponent,
