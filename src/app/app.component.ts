f41e8838fe4bdb7194fd96526cb580528ea30cd8
Jean Saunie <jean.saunie@devinci.fr>
2018-10-11 23:44:19 +0200

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>CanActivateChild</h1>
+      <h2>Checking child route access</h2>
+
+      <div class="nav">
+        <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact:true}">Home</a>
+        <a routerLink="/admin" routerLinkActive="active">Admin</a>
+      </div>
+
+      <router-outlet></router-outlet>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'guards';
